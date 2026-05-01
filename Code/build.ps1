04294c2177978b53deb1251eea46d0f80f7a7db5
OV975 <omarvilla920@gmail.com>
2026-05-01 10:01:08 -0700

    Delete Code/build.ps1

--- Code/build.ps1
+++
@@ -1,8 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-if (-not (Test-Path out)) {
-    New-Item -ItemType Directory -Path out | Out-Null
-}
-
-javac -d out src\canvascalendar\*.java src\canvascalendar\model\*.java src\canvascalendar\storage\*.java src\canvascalendar\ui\*.java
-Write-Host "Build completed."
